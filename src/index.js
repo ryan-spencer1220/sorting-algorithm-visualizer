4d3947d86a164a8487b0e3d7f4d61bf6a39ca4be
plymatea <andy.plymate@gmail.com>
2022-02-14 14:38:06 -0800

    add array size slider, and sorting method dropdown.
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>
    Co-authored-by: Timedog <Cruduper@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css'; 
+import { randomArray } from './js/business-logic.js';
 
-let array = [] 
-let arrayRange = $("#array-range").val()
-console.log(arrayRange)
-for (let i=10; i<arrayRange; i++) {
-  array.push(i)
+/*eslint-disable */
+
+function clearFields() {
+  array = [];
+  $('#bars').html('');
+};
+
+function barGeneration(array) {
+  for (let i = 0; i < array.length; i ++) {
+    $('#bars').append(`<div class="bar" id="bar-index-${i}"></div>`);
+    $(`#bar-index-${i}`).css("height", `${array[i]*2}px`);
+  }
 }
 
-const bar = $('<div></div>')
-bar.css("height", `"${array[0]*2}"px`)
+let arrayRange = $("#array-range").val();
+let array = randomArray(arrayRange);
+barGeneration(array);
 
-for (let i = 0; i < array.length; i ++) {
-  $('#bars').append(`<div class="bar" id="bar-index-${i}"></div>`);
-  $(`#bar-index-${i}`).css("height", `${array[i]*2}px`);
-}
 
+$('#array-range').on("input", function(){
+  clearFields();
+  arrayRange = $("#array-range").val();
+  array = randomArray(arrayRange);
+  barGeneration(array);
+})
+
+$('#sort-select').on("change", function(){
+
+})
+
+/*eslint-enable */
