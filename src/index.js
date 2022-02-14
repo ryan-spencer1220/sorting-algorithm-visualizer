c1f55981d0ff2d058c97cce0cf424c94804d552b
plymatea <andy.plymate@gmail.com>
2022-02-14 11:35:41 -0800

    create a bar graph from an array.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css'; 
+
+let array = [] 
+let arrayRange = $("#array-range").val()
+console.log(arrayRange)
+for (let i=10; i<arrayRange; i++) {
+  array.push(i)
+}
+
+
+const bars = $('#bars');
+
+const bar = $('<div></div>')
+bar.css("height", `"${array[0]*2}"px`)
+
+for (let i = 0; i < array.length; i ++) {
+  $('#bars').append(`<div class="bar" id="bar-index-${i}"></div>`);
+  $(`#bar-index-${i}`).css("height", `${array[i]*2}px`);
+}
+
